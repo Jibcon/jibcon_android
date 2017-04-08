db696254a3112570cb43f523b6a42ff0eb17995d
ChanJoo <sksckswn@naver.com>
2017-04-09 00:54:00 +0900

    Slack GitHook Test @2

--- app/src/main/java/com/example/admin/jipcon2/MainActivity.java
+++ app/src/main/java/com/example/admin/jipcon2/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity
     ViewPager vp;
     String[] drawer_str = {"about Jibcon", "문의", "알림 설정", "외출", "연결된 디바이스"};//사이드바 임시 메뉴 껍데기
     GlobalApplication app;
-
+    // chanjoo branch test
     // 변경
     private class pagerAdapter extends FragmentStatePagerAdapter{
         public pagerAdapter(android.support.v4.app.FragmentManager fm){
