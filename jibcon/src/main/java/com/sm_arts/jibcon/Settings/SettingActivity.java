d02f9012fe74a3e705bb1f48ef4ac63ded48d6ad
kimwoojin <94rladnwls@naver.com>
2017-05-20 16:50:53 +0900

    add ListView on SettingActivity

--- jibcon/src/main/java/com/sm_arts/jibcon/Settings/SettingActivity.java
+++ jibcon/src/main/java/com/sm_arts/jibcon/Settings/SettingActivity.java
@@ -3,21 +3,30 @@ package com.sm_arts.jibcon.Settings;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.sm_arts.jibcon.R;
 import com.tsengvn.typekit.TypekitContextWrapper;
 
-/**
- * Created by woojinkim on 2017. 5. 20..
- */
+
 
 public class SettingActivity extends AppCompatActivity {
+
+    ListView mSettingLv;
+    static final String[] mSettingList={"개인/보안","알림","고객센터"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, mSettingList);
+
+        mSettingLv = (ListView)findViewById(R.id.Lv_setting);
+        mSettingLv.setAdapter(adapter);
+
     }
 
 
