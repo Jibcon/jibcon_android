91c70d9f725fd4be5c21700e21847eef187ca4a7
kimwoojin <94rladnwls@naver.com>
2017-05-20 17:01:17 +0900

    make all classes

--- jibcon/src/main/java/com/sm_arts/jibcon/Settings/SettingActivity.java
+++ jibcon/src/main/java/com/sm_arts/jibcon/Settings/SettingActivity.java
@@ -3,6 +3,8 @@ package com.sm_arts.jibcon.Settings;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -22,11 +24,26 @@ public class SettingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
 
+        /* add String[] to ListView*/
         ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, mSettingList);
 
         mSettingLv = (ListView)findViewById(R.id.Lv_setting);
         mSettingLv.setAdapter(adapter);
+        /**/
 
+        /* add onItemClickListener to ListView*/
+        mSettingLv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView parent, View v, int position, long id) {
+
+                // get TextView's Text.
+                String strText = (String) parent.getItemAtPosition(position) ;
+
+            }
+        }) ;
+
+
+        /**/
     }
 
 
---
+++ jibcon/src/main/java/com/sm_arts/jibcon/Settings/UserCenter/JibconAsk.java
@@ -0,0 +1,8 @@
+package com.sm_arts.jibcon.Settings.UserCenter;
+
+/**
+ * Created by woojinkim on 2017. 5. 20..
+ */
+
+public class JibconAsk {
+}
