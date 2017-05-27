ef1abde7ef7af91194e9de76d1812fa243c0fc54
kimwoojin <94rladnwls@naver.com>
2017-05-27 14:46:49 +0900

    add arrow_back_white on mipmap

--- jibcon/src/main/java/com/sm_arts/jibcon/Settings/alarm/Alarm.java
+++ jibcon/src/main/java/com/sm_arts/jibcon/Settings/alarm/Alarm.java
@@ -47,8 +47,6 @@ public class Alarm extends AppCompatActivity {
             }
         }) ;
 
-
-        /**/
     }
 
 
--- jibcon/src/main/java/com/sm_arts/jibcon/Settings/person_secure/Person_Secure.java
+++ jibcon/src/main/java/com/sm_arts/jibcon/Settings/person_secure/Person_Secure.java
@@ -43,8 +43,6 @@ public class Person_Secure extends AppCompatActivity {
             }
         }) ;
 
-
-        /**/
     }
 
 
