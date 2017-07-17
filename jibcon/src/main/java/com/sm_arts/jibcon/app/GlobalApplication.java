be11625fc0111869bff5981c64ea04cff42348aa
JeongEuiJoon <dordong327@gmail.com>
2017-07-17 15:37:37 +0900

    review 반영

--- jibcon/src/main/java/com/sm_arts/jibcon/app/GlobalApplication.java
+++ jibcon/src/main/java/com/sm_arts/jibcon/app/GlobalApplication.java
@@ -71,10 +71,10 @@ public class GlobalApplication extends MultiDexApplication {
 
     public static OAuthLogin getNaverOAuthLogin()
     {
-        if(mOAuthLoginModule==null)
+        if(mOAuthLoginModule == null)
         {
-            mOAuthLoginModule=OAuthLogin.getInstance();
-            mOAuthLoginModule.init(getGlobalApplicationContext(),"f2VZgOgRx7HzZaCUzN_D","uldOlbBiia","JIBCON");
+            mOAuthLoginModule = OAuthLogin.getInstance();
+            mOAuthLoginModule.init(getGlobalApplicationContext(), "f2VZgOgRx7HzZaCUzN_D" , "uldOlbBiia" , "JIBCON");
 
         }
 
