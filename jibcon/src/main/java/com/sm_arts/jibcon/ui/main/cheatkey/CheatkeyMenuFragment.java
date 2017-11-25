d9dc94953aa38f9f1b62ab1bec1eb3b156998bd0
kimwoojin <94rladnwls@naver.com>
2017-11-25 13:09:32 +0900

    pr menu fragment accent 추가

--- jibcon/src/main/java/com/sm_arts/jibcon/ui/main/cheatkey/CheatkeyMenuFragment.java
+++ jibcon/src/main/java/com/sm_arts/jibcon/ui/main/cheatkey/CheatkeyMenuFragment.java
@@ -53,25 +53,67 @@ public class CheatkeyMenuFragment extends Fragment{
 
         mVp.setAdapter(new pagerAdapter(getChildFragmentManager())); // getSupportFragmentManager에서 수정
         mVp.setCurrentItem(0);
+        btn_active.setBackground(getResources().getDrawable(R.drawable.pr_bot_shadow_blue));
+        btn_passive.setBackground(getResources().getDrawable(R.drawable.pr_bot_shadow_gray));
 
         Log.d(TAG, "onCreateView: 4");
         btn_active.setOnClickListener(movePageListener);
         btn_active.setTag(0);
+
+
         btn_passive.setOnClickListener(movePageListener);
         btn_passive.setTag(1);
-
         Log.d(TAG, "onCreateView: 5");
         /* ↑Acitve & Passive Button↑ */
 
+        mVp.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                Log.d(TAG, "onPageScrolled: "+position);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                Log.d(TAG, "onPageSelected: "+position);
+                if(position==0) {
+                    btn_active.setBackground(getResources().getDrawable(R.drawable.pr_bot_shadow_blue));
+                    btn_passive.setBackground(getResources().getDrawable(R.drawable.pr_bot_shadow_gray));
+                } else {
+                    btn_active.setBackground(getResources().getDrawable(R.drawable.pr_bot_shadow_gray));
+                    btn_passive.setBackground(getResources().getDrawable(R.drawable.pr_bot_shadow_blue));
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                Log.d(TAG, "onPageScrollStateChanged: "+state);
+            }
+        });
         return layout;
     }
 
     /* ↓뷰 페이저(액티비티 슬라이드)↓ */
-    View.OnClickListener movePageListener = new View.OnClickListener() {
+    private View.OnClickListener movePageListener = new View.OnClickListener() {
         @Override
         public void onClick(View v){
             int tag = (int) v.getTag();
+            Log.d(TAG, "onClick: asd");
+            if(tag==0) {
+                btn_active.setBackground(getResources().getDrawable(R.drawable.pr_bot_shadow_blue));
+                btn_passive.setBackground(getResources().getDrawable(R.drawable.pr_bot_shadow_gray));
+                Log.d("asd","doingactive");
+            } else  {
+                btn_active.setBackground(getResources().getDrawable(R.drawable.pr_bot_shadow_gray));
+                btn_passive.setBackground(getResources().getDrawable(R.drawable.pr_bot_shadow_blue));
+                Log.d("asd","doingpassive");
+            }
+
             mVp.setCurrentItem(tag);
+
+
+
+
+
         }
     };
 
@@ -83,7 +125,7 @@ public class CheatkeyMenuFragment extends Fragment{
         @Override
         public Fragment getItem(int position) {
             Log.w(TAG, "movePageListener");
-
+            Log.d(TAG, "getItem: asdasd");
             if (position == 0) {
                 Log.d(TAG, "getItem: CheatKeyActiveFragment");
                 return new CheatkeyActiveFragment();
