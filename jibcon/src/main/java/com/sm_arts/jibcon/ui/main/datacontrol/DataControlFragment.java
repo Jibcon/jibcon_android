300c71b3660991a1aa54c01c909ea93407f904df
kimwoojin <94rladnwls@naver.com>
2017-12-03 13:25:38 +0900

    datacontrol temp image insertion

--- jibcon/src/main/java/com/sm_arts/jibcon/ui/main/datacontrol/DataControlFragment.java
+++ jibcon/src/main/java/com/sm_arts/jibcon/ui/main/datacontrol/DataControlFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.sm_arts.jibcon.R;
@@ -36,5 +37,4 @@ public class DataControlFragment extends android.support.v4.app.Fragment {
 
         return view;
     }
-
 }
